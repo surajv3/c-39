45208f22dbafc4e14c51ca4b20975221085c2be2
surajv3 <surajv3@yahoo.com>
2021-02-25 18:43:04 -0800

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,68 @@
+class Form{
+constructor(){
+
+this.input=createInput("Player Name: ")
+this.button=createButton("Start Race")
+this.greeting=createElement("h1")
+
+
+}
+hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+
+}
+
+display(){
+var title=createElement("h2");
+title.html("Car Racing");
+title.position(displayWidth/2,0);
+
+
+this.input.position(displayWidth/2-40,displayHeight/2-80);
+
+
+this.button.position(displayWidth/2+30,displayHeight/2);
+
+this.button.mousePressed(()=>{
+this.input.hide();
+this.button.hide();
+player.name = this.input.value();
+playerCount+=1
+player.index=playerCount;
+player.update();
+player.updateCount(playerCount);
+
+
+this.greeting.html("Welcome To The Race "+player.name)
+this.greeting.position(displayWidth/2-70,displayHeight/4)
+
+
+})
+
+}
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
